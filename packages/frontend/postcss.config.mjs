e80314c422c40c65081317652cec74b06868f55a
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2024-11-23 20:42:57 -0500

    fix: fix lint issue in postcss.config

--- packages/frontend/postcss.config.mjs
+++ packages/frontend/postcss.config.mjs
@@ -1,8 +1,10 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
     tailwindcss: {}
-  },
+  }
 };
 
 export default config;
