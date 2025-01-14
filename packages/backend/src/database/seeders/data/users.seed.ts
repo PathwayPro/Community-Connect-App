7fcadd7575abeb1c8fe2e0c232a83b44a9696278
Nicolas Fracchia <dev.nfracchia@gmail.com>
2025-01-13 18:07:55 -0700

    chore(seeder): for-all-modules

---
+++ packages/backend/src/database/seeders/data/users.seed.ts
@@ -0,0 +1,30 @@
+import { mentors, users } from "@prisma/client";
+
+const GeneralUsersData = {
+    middle_name: "Password123!",
+    password_hash: "$2b$10$Kf9ZbgWNeJX8HT9we5l4K.fMUdrPQEv8WRwYuQDEGoYoEelvntSdG",
+    dob: undefined,
+    show_dob: false,
+    arrival_in_canada: undefined,
+    goal_id: 0,
+    verification_token: "",
+    email_verified: false,
+    refresh_token: "",
+    last_logout: new Date()
+};
+
+export const UsersData:users[] = [
+    {id: 1,first_name: "U test",last_name: "User1",email: "user1@sv.com",role:"USER",...GeneralUsersData},
+    {id: 3,first_name: "U test",last_name: "User2",email: "user2@sv.com",role:"USER",...GeneralUsersData},
+    {id: 7,first_name: "U-M test",last_name: "User w Mentor",email: "um@sv.com",role:"USER",...GeneralUsersData},
+    {id: 8,first_name: "M test",last_name: "Mentor1",email: "mentor1@sv.com",role:"MENTOR",...GeneralUsersData},
+    {id: 9,first_name: "M test",last_name: "Mento2",email: "mentor2@sv.com",role:"MENTOR",...GeneralUsersData},
+    {id: 10,first_name: "A test",last_name: "Admin1",email: "admin1@sv.com",role:"ADMIN",...GeneralUsersData},
+    {id: 11,first_name: "A test",last_name: "Admin2",email: "admin2@sv.com",role:"ADMIN",...GeneralUsersData},
+];
+
+export const MentorsData:mentors[] = [
+    {id: 1,max_mentees: 20,availability: "Mondays around 8PM MST",has_experience: true,experience_details: "I have experience...",status: "PENDING",user_id: 7},
+    {id: 2,max_mentees: 50,availability: "Tuesday to Saturday, anytime",has_experience: true,experience_details: "Some experience details...",status: "APPROVED",user_id: 8},
+    {id: 5,max_mentees: 50,availability: "EDITED: Monday to Friday after 3PM MST",has_experience: true,experience_details: "Some details about a experience",status: "APPROVED",user_id: 9},
+];
---
+++ packages/backend/src/database/seeders/seeder.ts
@@ -0,0 +1,196 @@
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { UsersData, MentorsData } from './data/users.seed';
+import { EventsCategoriesData, EventsData, EventsInvitationsData, EventsManagersData, EventsSubscriptionsData, EventsSubscriptionsUpdatesData } from './data/events.seed';
+import { NewsData } from './data/news.seed';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+const emptyTables = async () => {
+    try {
+        // DELETING IN ORDER TO PREVENT FK ERRORS
+        console.log('\n\n 1. DELETING DATA (IN ORDER) TO PREVENT FK ERRORS:\n');
+
+        // NEWS
+        const newsStatus = (await prisma.news.deleteMany({})) ? 'OK' : 'ERROR';
+        console.log(`     1.1 NEWS (${newsStatus}!)`);
+        
+        // EVENTS SUBSCRIPTIONS UPDATES
+        const eventsSubscriptionsUpdatesStatus = (await prisma.eventsSubscriptionsUpdates.deleteMany({})) ? 'OK' : 'ERROR';
+        console.log(`     1.2 EVENTS SUBSCRIPTIONS UPDATES (${eventsSubscriptionsUpdatesStatus}!)`);
+        
+        // EVENTS SUBSCRIPTIONS UPDATES
+        const eventsSubscriptionsStatus = (await prisma.eventsSubscriptions.deleteMany({})) ? 'OK' : 'ERROR';
+        console.log(`     1.3 EVENTS SUBSCRIPTIONS (${eventsSubscriptionsStatus}!)`);
+
+        // EVENTS INVITATIONS
+        const eventsInvitationsStatus = (await prisma.eventsInvitations.deleteMany({})) ? 'OK' : 'ERROR';
+        console.log(`     1.4 EVENTS INVITATIONS (${eventsInvitationsStatus}!)`);
+
+        // EVENTS MANAGERS
+        const eventsManagersStatus = (await prisma.eventsManagers.deleteMany({})) ? 'OK' : 'ERROR';
+        console.log(`     1.5 EVENTS MANAGERS (${eventsManagersStatus}!)`);
+
+        // EVENTS
+        const eventsStatus = (await prisma.events.deleteMany({})) ? 'OK' : 'ERROR';
+        console.log(`     1.6 EVENTS (${eventsStatus}!)`);
+
+        // EVENTS CATEGORIES
+        const eventsCategoriesStatus = (await prisma.eventsCategories.deleteMany({})) ? 'OK' : 'ERROR';
+        console.log(`     1.7 EVENTS CATEGORIES (${eventsCategoriesStatus}!)`);
+
+        // MENTORS
+        const mentorsStatus = (await prisma.mentors.deleteMany({})) ? 'OK' : 'ERROR';
+        console.log(`     1.8 MENTORS (${mentorsStatus}!)`);
+
+        // USERS
+        const usersStatus = (await prisma.users.deleteMany({})) ? 'OK' : 'ERROR';
+        console.log(`     1.9 USERS (${usersStatus}!)`);
+
+
+    }catch(error){
+        throw new InternalServerErrorException('| - - - - - > An error ocurred deleting records.')
+    }
+}
+
+const seedUsers = async () => {
+    try{
+        console.log(`\n\n\n 2. SEEDING TABLE USERS:   (${UsersData.length} records)\n`)
+        for(const user of UsersData){
+            (await prisma.users.create({data: user})) ?  
+            console.log(`     USER ID: ${user.id} created - - - - - - - > OK! `) :
+            console.log(`     USER ID: ${user.id} created - - - - - - - > ERROR! `);
+        }
+    }catch(error){
+        throw new InternalServerErrorException('There was an error seeding users:', error)
+    }
+}
+const seedMentors = async () => {
+    try{
+        console.log(`\n\n\n 3. SEEDING TABLE MENTORS:   (${MentorsData.length} records)\n`)
+        for(const mentor of MentorsData){
+            (await prisma.mentors.create({data: mentor})) ? 
+            console.log(`     MENTOR ID: ${mentor.id} created - - - - - - - > OK! `) :
+            console.log(`     MENTOR ID: ${mentor.id} created - - - - - - - > ERROR! `);
+        }
+    }catch(error){
+        throw new InternalServerErrorException('There was an error seeding mentors:', error)
+    }
+
+    return true;
+}
+const seedEventsCategories = async () => {
+    try{
+        console.log(`\n\n\n 3. SEEDING TABLE EVENTS CATEGORIES:   (${EventsCategoriesData.length} records)\n`)
+        for(const category of EventsCategoriesData){
+            (await prisma.eventsCategories.create({data: category})) ? 
+            console.log(`     EVENT CATEGORY ID: ${category.id} created - - - - - - - > OK! `) :
+            console.log(`     EVENT CATEGORY  ID: ${category.id} created - - - - - - - > ERROR! `);
+        }
+    }catch(error){
+        throw new InternalServerErrorException('There was an error seeding events categories:', error)
+    }
+
+    return true;
+}
+const seedEvents = async () => {
+    try{
+        console.log(`\n\n\n 4. SEEDING TABLE EVENTS:   (${EventsData.length} records)\n`)
+        for(const ev of EventsData){
+            (await prisma.events.create({data: ev})) ? 
+            console.log(`     EVENT ID: ${ev.id} created - - - - - - - > OK! `) :
+            console.log(`     EVENT ID: ${ev.id} created - - - - - - - > ERROR! `);
+        }
+    }catch(error){
+        throw new InternalServerErrorException('There was an error seeding events:', error)
+    }
+
+    return true;
+}
+const seedEventsManagers = async () => {
+    try{
+        console.log(`\n\n\n 5. SEEDING TABLE EVENTS MANAGERS:   (${EventsManagersData.length} records)\n`)
+        for(const em of EventsManagersData){
+            (await prisma.eventsManagers.create({data: em})) ? 
+            console.log(`     EVENT MANAGER ID: ${em.id} created - - - - - - - > OK! `) :
+            console.log(`     EVENT MANAGER ID: ${em.id} created - - - - - - - > ERROR! `);
+        }
+    }catch(error){
+        throw new InternalServerErrorException('There was an error seeding events managers:', error)
+    }
+
+    return true;
+}
+const seedEventsInvitations = async () => {
+    try{
+        console.log(`\n\n\n 6. SEEDING TABLE EVENTS INVITATIONS:   (${EventsInvitationsData.length} records)\n`)
+        for(const ei of EventsInvitationsData){
+            (await prisma.eventsInvitations.create({data: ei})) ? 
+            console.log(`     EVENT INVITATION ID: ${ei.id} created - - - - - - - > OK! `) :
+            console.log(`     EVENT INVITATION ID: ${ei.id} created - - - - - - - > ERROR! `);
+        }
+    }catch(error){
+        throw new InternalServerErrorException('There was an error seeding events invitations:', error)
+    }
+
+    return true;
+}
+const seedEventsSubscriptions = async () => {
+    try{
+        console.log(`\n\n\n 7. SEEDING TABLE EVENTS SUBSCRIPTIONS:   (${EventsSubscriptionsData.length} records)\n`)
+        for(const es of EventsSubscriptionsData){
+            (await prisma.eventsSubscriptions.create({data: es})) ? 
+            console.log(`     EVENT SUBSCRIPTION ID: ${es.id} created - - - - - - - > OK! `) :
+            console.log(`     EVENT SUBSCRIPTION ID: ${es.id} created - - - - - - - > ERROR! `);
+        }
+    }catch(error){
+        throw new InternalServerErrorException('There was an error seeding events subscriptions:', error)
+    }
+
+    return true;
+}
+const seedEventsSubscriptionsUpdates = async () => {
+    try{
+        console.log(`\n\n\n 8. SEEDING TABLE EVENTS SUBSCRIPTIONS UPDATES:   (${EventsSubscriptionsUpdatesData.length} records)\n`)
+        for(const esu of EventsSubscriptionsUpdatesData){
+            (await prisma.eventsSubscriptionsUpdates.create({data: esu})) ? 
+            console.log(`     EVENT SUBSCRIPTION UPDATE ID: ${esu.id} created - - - - - - - > OK! `) :
+            console.log(`     EVENT SUBSCRIPTION UPDATE ID: ${esu.id} created - - - - - - - > ERROR! `);
+        }
+    }catch(error){
+        throw new InternalServerErrorException('There was an error seeding events subscriptions updates:', error)
+    }
+
+    return true;
+}
+const seedNews = async () => {
+    try{
+        console.log(`\n\n\n 9. SEEDING TABLE NEWS:   (${NewsData.length} records)\n`)
+        for(const n of NewsData){
+            (await prisma.news.create({data: n})) ? 
+            console.log(`     NEWS ID: ${n.id} created - - - - - - - > OK! `) :
+            console.log(`     NEWS ID: ${n.id} created - - - - - - - > ERROR! `);
+        }
+    }catch(error){
+        throw new InternalServerErrorException('There was an error seeding news:', error)
+    }
+
+    return true;
+}
+
+const seedAll = async () => {
+    console.log('\n\n         - - - - - - - - - - - - - - \n       | R U N N I N G   S E E D E R | \n         - - - - - - - - - - - - - - \n\n')
+
+    await emptyTables()
+    await seedUsers()
+    await seedMentors()
+    await seedEventsCategories()
+    await seedEvents()
+    await seedEventsManagers()
+    await seedEventsInvitations()
+    await seedEventsSubscriptions()
+    await seedEventsSubscriptionsUpdates()
+    await seedNews()
+}
+
+seedAll();
