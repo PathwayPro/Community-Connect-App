47cb7db0f980e21821c5e41153be4bd0c5b2fefd
nicolasfracchia <nfracchia@hotmail.com>
2025-01-31 19:26:33 -0700

    docs(interests): complete-documentation

--- packages/backend/src/interests/entities/interest.entity.ts
+++ packages/backend/src/interests/entities/interest.entity.ts
@@ -1,1 +1,16 @@
-export class Interest {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsInt, IsNotEmpty } from 'class-validator';
+
+export class Interest {
+  @ApiProperty({
+    description: 'Name for the interest',
+    example: 'Networking',
+  })
+  @IsInt()
+  id: number;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+}
--- packages/backend/src/interests/interests.controller.ts
+++ packages/backend/src/interests/interests.controller.ts
@@ -1,25 +1,57 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { InterestsService } from './interests.service';
 import { CreateInterestDto } from './dto/create-interest.dto';
 import { UpdateInterestDto } from './dto/update-interest.dto';
-import { GetUser, Public, Roles } from 'src/auth/decorators';
+import { Public, Roles } from 'src/auth/decorators';
 import { JwtAuthGuard, RolesGuard } from 'src/auth/guards';
-import { JwtPayload } from 'src/auth/util/JwtPayload.interface';
-import { InterestToUserDto } from './dto/interest-to-user.dto';
+import {
+  ApiTags,
+  ApiBody,
+  ApiOperation,
+  ApiCreatedResponse,
+  ApiInternalServerErrorResponse,
+  ApiBearerAuth,
+  ApiParam,
+  ApiOkResponse,
+  ApiNotFoundResponse,
+} from '@nestjs/swagger';
+import { Interest } from './entities/interest.entity';
 
 @UseGuards(JwtAuthGuard, RolesGuard)
-
+@ApiTags('Interests')
 @Controller('interests')
 export class InterestsController {
   constructor(private readonly interestsService: InterestsService) {}
 
   @Post()
   @Roles('ADMIN')
+  @ApiBody({ type: CreateInterestDto })
+  @ApiCreatedResponse({ type: Interest })
+  @ApiInternalServerErrorResponse({
+    description: 'Error creating the interest: [ERROR MESSAGE]',
+  })
+  @ApiOperation({
+    summary: 'Create interests record',
+    description:
+      'Creates interest or throws Internal Server Error Exception. \n\n REQUIRED ROLES: **ADMIN**',
+  })
+  @ApiBearerAuth()
   create(@Body() createInterestDto: CreateInterestDto) {
-    const interest: CreateInterestDto = {name: createInterestDto.name};
+    const interest: CreateInterestDto = { name: createInterestDto.name };
     return this.interestsService.create(interest);
   }
 
+  /*
+  NO NEED FOR THIS ONE, WILL BE INTEGRATED IN USERS AND MENTORS
   @Post('/user')
   @Roles('ADMIN', 'MENTOR', 'USER')
   addInterestToUser(@GetUser() user: JwtPayload, @Body('interest_id') interest_id: number) {
@@ -29,26 +61,63 @@ export class InterestsController {
     };
     return this.interestsService.addInterestToUser(addInterest);
   }
+  */
 
   @Get()
   @Public()
+  @ApiOkResponse({ type: Interest, isArray: true })
+  @ApiInternalServerErrorResponse({
+    description: 'Error fetching interests: [ERROR MESSAGE]',
+  })
+  @ApiOperation({
+    summary: 'Fetch interests',
+    description: 'Fetch all interests. \n\n REQUIRED ROLES: **PUBLIC**',
+  })
   findAll() {
     return this.interestsService.findAll();
   }
 
   @Get(':id')
   @Public()
+  @ApiParam({ name: 'id' })
+  @ApiOkResponse({ type: Interest })
+  @ApiInternalServerErrorResponse({
+    description: 'Error fetching interest: [ERROR MESSAGE]',
+  })
+  @ApiNotFoundResponse({ description: 'There is no Interest with ID #[:id]' })
+  @ApiOperation({
+    summary: 'Fetch interest by ID',
+    description: 'Fetch one interest by ID. \n\n REQUIRED ROLES: **PUBLIC**',
+  })
   findOne(@Param('id') id: string) {
     return this.interestsService.findOne(+id);
   }
 
   @Patch(':id')
   @Roles('ADMIN')
-  update(@Param('id') id: string, @Body() updateInterestDto: UpdateInterestDto) {
-    const interest: UpdateInterestDto = {name: updateInterestDto.name};
+  @ApiBody({ type: UpdateInterestDto })
+  @ApiParam({ name: 'id' })
+  @ApiOkResponse({ type: Interest })
+  @ApiNotFoundResponse({ description: 'There is no Interest with ID #[:id]' })
+  @ApiInternalServerErrorResponse({
+    description: 'Error updating interest with ID #[:id]: [ERROR MESSAGE]',
+  })
+  @ApiOperation({
+    summary: 'Update interests',
+    description:
+      'Update interests (only name) with ID [:id]. \n\n REQUIRED ROLES: **ADMIN**',
+  })
+  @ApiBearerAuth()
+  update(
+    @Param('id') id: string,
+    @Body() updateInterestDto: UpdateInterestDto,
+  ) {
+    const interest: UpdateInterestDto = { name: updateInterestDto.name };
     return this.interestsService.update(+id, interest);
   }
 
+  /*
+  NO NEED FOR THIS ONE, WILL BE INTEGRATED IN USERS AND MENTORS
   @Delete('/user/:interestId')
   @Roles('ADMIN', 'MENTOR', 'USER')
   removeInterestFromUser(@GetUser() user: JwtPayload, @Param('interestId') interest_id: string) {
@@ -58,9 +127,18 @@ export class InterestsController {
     };
     return this.interestsService.removeInterestFromUser(removeInterest);
   }
+*/
 
   @Delete(':id')
   @Roles('ADMIN')
+  @ApiOkResponse({ type: Number })
+  @ApiParam({ name: 'id' })
+  @ApiOkResponse({ type: Number })
+  @ApiOperation({
+    summary: '**[PENDING]**',
+    description:
+      'REMOVE INTEREST OR KEEP IT "UNAVAILABLE" TO PRESERVE INFORMATION?. \n\n REQUIRED ROLES: **ADMIN**',
+  })
   remove(@Param('id') id: string) {
     return id;
     //return this.interestsService.remove(+id);
