d34258448217877f709125b40a83e7895252e7ec
nfracchia-pw <dev.nfracchia@gmail.com>
2025-03-08 20:55:25 -0700

    chore(auth): imports from direct files

--- packages/backend/src/auth/auth.module.ts
+++ packages/backend/src/auth/auth.module.ts
@@ -1,7 +1,7 @@
 import { Module, forwardRef } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { AuthService } from './services';
+import { AuthService } from './services/auth.service';
 import { PrismaService } from '../database/prisma.service';
 import { AuthController } from './auth.controller';
 import { EmailService } from './services/email.service';
