4ea2b2e7dafad2724a0f22b6dfa7f1a188487305
nfracchia-pw <dev.nfracchia@gmail.com>
2025-03-29 18:29:23 -0600

    docs(networking): documentation for complete flow

--- packages/backend/src/networking/dto/filter-networking.dto.ts
+++ packages/backend/src/networking/dto/filter-networking.dto.ts
@@ -1,22 +1,44 @@
-import { ConnectionRequestsStatus } from "@prisma/client";
-import { IsDateString, IsOptional, IsString } from "class-validator"
+import { ConnectionRequestsStatus } from '@prisma/client';
+import { IsDateString, IsEnum, IsInt, IsOptional } from 'class-validator';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
 
 export class FilterConnectionRequestsDto {
-    @IsString()
-    @IsOptional()
-    user_id?: string; // ID of the other user, not the logged one
-    
-    @IsString()
-    @IsOptional()
-    status?: ConnectionRequestsStatus;
+  @ApiPropertyOptional({
+    description: 'Request from a specific user',
+    example: 10,
+  })
+  @IsInt()
+  @IsOptional()
+  @Type(() => Number)
+  user_id?: number;
 
-    @IsDateString()
-    @IsOptional()
-    date_from?: Date; // Format ISO 8601
+  @ApiPropertyOptional({
+    description:
+      'Status of the connection request [ PENDING | APPROVED | REJECTED]',
+    example: 'APPROVED',
+  })
+  @IsEnum(ConnectionRequestsStatus)
+  @IsOptional()
+  status?: ConnectionRequestsStatus;
 
-    @IsDateString()
-    @IsOptional()
-    date_to?: Date; // Format ISO 8601
+  @ApiPropertyOptional({
+    description:
+      '`created_at` greater than inserted date, ignoring time. `Y-m-d 00:00:00.000`',
+    example: '2025-01-02',
+  })
+  @IsDateString()
+  @IsOptional()
+  date_from?: Date; // Format ISO 8601
+
+  @ApiPropertyOptional({
+    description:
+      '`created_at` lower than inserted date, ignoring time. `Y-m-d 59:59:59.999`',
+    example: '2025-01-05',
+  })
+  @IsDateString()
+  @IsOptional()
+  date_to?: Date; // Format ISO 8601
 }
 
 export class FilterConnectedUsersDto {}
