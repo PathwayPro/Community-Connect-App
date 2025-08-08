40b6cc9eaf5940f5a309c490e24b9cac6fd9f1db
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2025-08-07 22:32:59 -0400

    chore: fix auth responsiveness

--- packages/frontend/src/app/auth/reset-password/page.tsx
+++ packages/frontend/src/app/auth/reset-password/page.tsx
@@ -26,14 +26,17 @@ const ResetPasswordContent = () => {
   }, [router, searchParams]);
 
   return (
-    <div className="relative mx-auto flex min-h-screen w-full max-w-screen-xl items-center justify-between px-4">
+    <div className="relative mx-auto flex min-h-screen w-full max-w-screen-xl items-center justify-center px-4 sm:gap-8 md:gap-12 lg:gap-16">
       <div className="w-full sm:w-[420px]">
         <Suspense fallback={<div>Loading...</div>}>
           <ForgotPasswordForm />
         </Suspense>
       </div>
-      <div className="hidden h-full sm:block">
-        <Separator orientation="vertical" className="h-[70vh]" />
+      <div className="hidden sm:block">
+        <Separator
+          orientation="vertical"
+          className="mx-6 h-[70vh] md:mx-8 lg:mx-10"
+        />
       </div>
       <div className="hidden flex-1 sm:block">
         <AuthCarousel />
